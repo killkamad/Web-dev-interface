f046b611d64429125e25267b702c0634b2a00214
Kirill <killka_m@mail.ru>
2018-10-08 10:47:55 +0600

    da

--- 08.10.18/index.js
+++ 08.10.18/index.js
@@ -1,12 +1,29 @@
 var form = document.querySelector("form");
 var list = document.getElementById('list');
 var userInput = document.getElementById('user-todo');
-console.log(form);
+
+var todos = localStorage.getItem('todos') ?
+   JSON.parse(localStorage.getItem('todos')) :
+   [];
+
+console.log(todos);
+
+var makeItem = (item) => {
+var li = document.createElement('li');
+li.textContent = item;
+li.style.color = 'red';
+list.appendChild(li);
+}
+
+todos.map(todo => {
+makeItem(todo);
+});
+
 
 form.addEventListener("submit",function(event){
 event.preventDefault();
-var li = document.createElement('li');
-li.textContent = userInput.value;
-li.style.color = 'red';
-list.appendChild(li);
+todos.push(userInput.value);
+localStorage.setItem('todos', JSON.stringify(todos));
+makeItem(userInput.value);
+
 });
