53d582200daa216f706bf3ec8d379a3a7a377ee4
Kirill <killka_m@mail.ru>
2018-10-22 10:52:14 +0600

    da

---
+++ 22.10.18/converter.js
@@ -0,0 +1,17 @@
+switch(5){
+	case 1:
+	console.log('one');
+	break;
+	case 2:
+	console.log('one');
+	break;
+	case 3:
+	console.log('one');
+	break;
+	case 4:
+	console.log('one');
+	break;
+	case 5:
+	console.log('one');
+	break;
+}
