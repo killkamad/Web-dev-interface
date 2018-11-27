b2756fe2578b4371156381ce1c38df294d0abe1d
Kirill <killka_m@mail.ru>
2018-11-27 20:16:43 +0600

    Chatappp

---
+++ Chat/src/actions/index.js
@@ -0,0 +1,24 @@
+import * as types from '../constants/ActionTypes'
+
+let nextMessageId = 0
+// eslint-disable-next-line
+const nextUserId = 0
+
+export const addMessage = (message, author) => ({
+  type: types.ADD_MESSAGE,
+  id: nextMessageId++,
+  message,
+  author
+})
+
+export const messageReceived = (message, author) => ({
+  type: types.MESSAGE_RECEIVED,
+  id: nextMessageId++,
+  message,
+  author
+})
+
+export const populateUsersList = users => ({
+  type: types.USERS_LIST,
+  users
+})
